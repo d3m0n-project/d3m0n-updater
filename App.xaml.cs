718b9e6b8771c264d4b324fd10ed40c8fb208963
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2023-06-10 18:07:37 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace d3m0n_X1_updater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Windows.Input;
+
+
+
+namespace d3m0n_X1_updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		public static string outDrive;
+		
+        public MainWindow()
+        {
+            InitializeComponent();
+			
+			ServicePointManager.Expect100Continue = true; 
+			ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc00);
+			
+			DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    if(File.Exists(drive.RootDirectory+"INFO_UF2.TXT"))
+					{
+						Ports.Items.Add(drive.RootDirectory);
+						Ports.Text = drive.RootDirectory.ToString();
+					}
+                }
+            }	
+        }
+		private void install(object sender, RoutedEventArgs e)
+		{
+			Mouse.SetCursor(Cursors.Wait);
+			string outDrive = Ports.Text.ToString()+"new.uf2";
+			
+			if(outDrive == "new.uf2")
+			{
+				MessageBox.Show("Select your D3M0N device port to start downloading firmware");
+				Mouse.SetCursor(Cursors.Arrow);
+				return;
+			}
+			
+			try
+			{
+				new WebClient().DownloadFile("https://raw.githubusercontent.com/4RE5Team/D3M0N-X1/main/firmware/firmware.uf2", outDrive);
+			}
+			catch(Exception error)
+			{
+				Mouse.SetCursor(Cursors.Arrow);
+				MessageBox.Show(error.ToString());
+				return;
+			}
+			MessageBox.Show("D3M0N-X1 firmware is successfully installed!");
+			Mouse.SetCursor(Cursors.Arrow);
+		}
+		private void close(object sender, RoutedEventArgs e)
+		{
+			Mouse.SetCursor(Cursors.Arrow);
+			this.Close();
+		}
+    }
+}
